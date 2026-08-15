6fa240e886256c8413fbcc84eb8c82ee4f69d0cc
SrinidhiPasham <sriinidhi27@gmail.com>
2026-08-15 21:10:29 +0530

    Implement abstraction with Animal and Dog classes

---
+++ 05-OOP-Concepts/AbstractionExample.java
@@ -0,0 +1,26 @@
+abstract class Animal {
+
+    abstract void sound();
+
+    void sleep() {
+        System.out.println("Animal is sleeping");
+    }
+}
+
+class Dog extends Animal {
+
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class AbstractionExample {
+
+    public static void main(String[] args) {
+
+        Dog dog = new Dog();
+
+        dog.sound();
+        dog.sleep();
+    }
+}

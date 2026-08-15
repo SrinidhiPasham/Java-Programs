3c9eb7eaf04c5efff79e89165b940ebbb975d455
SrinidhiPasham <sriinidhi27@gmail.com>
2026-08-15 21:10:11 +0530

    Add polymorphism example with Animal and Dog classes

---
+++ 05-OOP-Concepts/PolymorphismExample.java
@@ -0,0 +1,23 @@
+class Animal {
+
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class PolymorphismExample {
+
+    public static void main(String[] args) {
+
+        Animal animal = new Dog();
+
+        animal.sound();
+    }
+}

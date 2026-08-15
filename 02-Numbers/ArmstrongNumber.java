e67e52b282372932e6e477f51008d9dbac6d203d
SrinidhiPasham <sriinidhi27@gmail.com>
2026-08-15 19:48:47 +0530

    Add ArmstrongNumber class to check Armstrong numbers

---
+++ 02-Numbers/ArmstrongNumber.java
@@ -0,0 +1,22 @@
+public class ArmstrongNumber {
+
+    public static void main(String[] args) {
+
+        int number = 153;
+        int originalNumber = number;
+        int sum = 0;
+
+        while (number != 0) {
+
+            int digit = number % 10;
+            sum = sum + (digit * digit * digit);
+            number = number / 10;
+        }
+
+        if (sum == originalNumber) {
+            System.out.println(originalNumber + " is an Armstrong Number");
+        } else {
+            System.out.println(originalNumber + " is not an Armstrong Number");
+        }
+    }
+}
